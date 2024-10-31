c5ac6db35017a5b8c474f3ea0be6d353eb1cbad9
MemmedzadeAygun <memmedzadeaygun2004@gmail.com>
2024-10-31 15:50:31 +0400

    Lesson20_task

---
+++ app.js
@@ -0,0 +1,16 @@
+let text=prompt("Enter the text: ");
+console.log("Text size:"+" "+text.length);  //task1
+
+console.log(text.includes("e"));            //task2
+
+console.log(text.startsWith("code"));       //task3
+
+console.log(text.endsWith("code"));         //task4
+
+console.log(text.replaceAll("t","####"));   //task5
+
+let number1=12345;
+let number2=98765;
+let result=(number1*number2).toString();
+console.log(result.charAt(5));              //task6
+
